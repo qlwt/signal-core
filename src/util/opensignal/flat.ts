060544fa28edbad5550c58f4a2cbd620e6a2d193
LerpanWortt <lerpanwortt@gmail.com>
2026-03-12 09:40:52 +0100

    add optimisation for flat signals

--- src/util/opensignal/flat.ts
+++ src/util/opensignal/flat.ts
@@ -2,17 +2,17 @@ import { batcher } from "#src/const/batcher.js";
 import type { ESignal } from "#src/esignal/type/ESignal.js";
 import type { OSignal } from "#src/osignal/type/OSignal.js";
 import { signal_sub_emit } from "#src/signal/sub/emit.js";
-import type { PickWrapperExpected } from "#src/type/PickWrapper.js";
+import type { PickWrapper, PickWrapper_Fail } from "#src/type/PickWrapper.js";
 import type { Signal_InferO } from "#src/type/signal/Infer.js";
 import type { Signal_Sub, Signal_SubConfig } from "#src/type/signal/Sub.js";
 import { attachment_new_lazy } from "#src/util/attachment/new/lazy.js";
 
-type Src_Generic = OSignal<PickWrapperExpected<ESignal>>
+type Src_Generic = OSignal<PickWrapper<ESignal> | PickWrapper_Fail>
 
 export type OpenSignalFlat<Src extends Src_Generic> = {
     identity(): Symbol
     target(): Signal_InferO<Src>
-    rmsub(sub: Signal_Sub): void
+    rmsub(sub: Signal_Sub, config?: Signal_SubConfig): void
     addsub(sub: Signal_Sub, config?: Signal_SubConfig): void
 }
 
@@ -31,9 +31,10 @@ export const opensignal_flat = function <Src extends Src_Generic>(src: Src): Ope
         if (cache) {
             if (batcher_id !== cache.batcher_id) {
                 const src_id = src.id()
-                const src_output = src.output() as Signal_InferO<Src>
 
                 if (cache.src_id !== src_id) {
+                    const src_output = src.output() as Signal_InferO<Src>
+
                     cache = {
                         src_id,
                         batcher_id,
@@ -57,7 +58,8 @@ export const opensignal_flat = function <Src extends Src_Generic>(src: Src): Ope
 
     const attachment = attachment_new_lazy({
         connection_new: order => {
-            let target_last: Signal_InferO<Src> | null = null
+            let last_srcid: Symbol | null = null
+            let last_target: Signal_InferO<Src> | null = null
 
             const emit = () => {
                 attachment.emit(order)
@@ -67,21 +69,24 @@ export const opensignal_flat = function <Src extends Src_Generic>(src: Src): Ope
                 const cache_l = cache_get()
                 const cache_now = cache_l.target
 
-                const last = target_last
+                const last = last_target
 
-                {
-                    target_last = cache_now
+                if (last_srcid !== cache_l.src_id) {
+                    {
+                        last_target = cache_now
+                        last_srcid = cache_l.src_id
+                    }
+
+                    if (last && last.pick) {
+                        last.value.rmsub(emit)
+                    }
+
+                    if (cache_now.pick) {
+                        cache_now.value.addsub(emit, { order })
+                    }
+
+                    signal_sub_emit(emit, { order: false })
                 }
-
-                if (last && last.pick) {
-                    last.value.rmsub(emit)
-                }
-
-                if (cache_now.pick) {
-                    cache_now.value.addsub(emit, { order })
-                }
-
-                signal_sub_emit(emit, { order: false })
             }
 
             return {
@@ -90,7 +95,8 @@ export const opensignal_flat = function <Src extends Src_Generic>(src: Src): Ope
                     const cache_now = cache_l.target
 
                     {
-                        target_last = cache_now
+                        last_target = cache_now
+                        last_srcid = cache_l.src_id
                     }
 
                     src.addsub(src_sub, { order })
@@ -103,8 +109,8 @@ export const opensignal_flat = function <Src extends Src_Generic>(src: Src): Ope
                 detach: () => {
                     src.rmsub(src_sub)
 
-                    if (target_last && target_last.pick) {
-                        target_last.value.rmsub(emit)
+                    if (last_target && last_target.pick) {
+                        last_target.value.rmsub(emit)
                     }
                 },
             }
